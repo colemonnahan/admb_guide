4c42553c3f7319f3564eed88ab47f34e1cb8ee78
colemonnahan <monnahc@uw.edu>
2014-08-01 13:38:41 -0700

    renamed guide folder and moved everything in there, also added bibliography to the guide

---
+++ auto/admb_guide.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook "admb_guide"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec:outfiles"
+     "sec:restart"
+     "sec:diag"
+     "sec:startvals"
+     "sec:MH"
+     "tab:mh_args"
+     "fig:mcrb"
+     "sec:hybrid"
+     "sec:sir")
+    (TeX-run-style-hooks
+     "float"
+     "graphicx"
+     "amssymb"
+     "amsmath"
+     "geometry"
+     "margin=1in"
+     "latex2e"
+     "art10"
+     "article"
+     "")))
+
